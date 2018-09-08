52c8475c6c55818351437589331640f4c02f767d
Henry Rodman <henry.rodman@gmail.com>
2018-09-08 10:30:24 -0500

    fix up rendering paths

--- render.R
+++ render.R
@@ -1,19 +1,21 @@
 local({
   knitr::opts_chunk$set(
-    fig.path   = "./assets/images/",
-    cache.path = "./cache/",
+    fig.path   = "../assets/images/",
+    cache.path = "../cache/",
     collapse = TRUE
   )
   
-  rmds = list.files('.', '[.]Rmd$', recursive = TRUE, full.names = TRUE)
+  rmds = list.files("./R", "[.]Rmd$", recursive = TRUE, full.names = TRUE)
   
   render_the_file <- function(file) {
-    fn <- tail(strsplit(file, '/')[[1]], 1)
-    outMarkdown <- gsub('.Rmd', '.md', fn)
+    fn <- tail(strsplit(file, "/")[[1]], 1)
+    outMarkdown <- gsub(".Rmd", ".md", fn)
     
-    outFile <- file.path('..', '_posts', outMarkdown)
+    outFile <- file.path(".", "_posts", outMarkdown)
     
-    rmarkdown::render(file, output_file = outFile)
+    rmarkdown::render(file, output_file = outMarkdown)
+    file.rename(file.path("./R", outMarkdown),
+                file.path("./_posts", outMarkdown))
   }
   
   lapply(rmds, render_the_file)
